efbc1ffa59e6b5471461f8351e058d7bb7a2dc4d
Giraffe24058 <all24058@byui.edu>
2025-04-25 16:04:41 -0600

    first draft for core requirement #1, still working

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percent = Console.ReadLine();
+        int g = int.Parse(percent);
+
+        if (90 < g)
+        {
+            Console.Write("Your letter grade is ");
+        }
+        else if()
+        {
+            Console.Write("Your letter grade is ");
+        }
+        else
+        {
+            Console.Write("Your letter grade is ");
+        }
     }
 }
