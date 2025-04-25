3dc35c392e1bd155a49632a5904b939c341c35ea
Giraffe24058 <all24058@byui.edu>
2025-04-25 16:13:56 -0600

    Add if else statements for determining whether or not they passed the class, with messages.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,17 +8,35 @@ class Program
         string percent = Console.ReadLine();
         int g = int.Parse(percent);
 
-        if (90 < g)
+        if (90 < g && g < 100)
         {
-            Console.Write("Your letter grade is ");
+            Console.Write("Your letter grade is A. ");
         }
-        else if()
+        else if(80 < g && g < 90)
         {
-            Console.Write("Your letter grade is ");
+            Console.Write("Your letter grade is B. ");
+        }
+        else if(70 < g && g < 80)
+        {
+            Console.Write("Your letter grade is C. ");
+        }
+        else if (60 < g && g < 70)
+        {
+            Console.Write("Your letter grade is D. ");
         }
         else
         {
-            Console.Write("Your letter grade is ");
+            Console.Write("Your letter grade is F. ");
+        }
+
+
+        if(g >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed the class! ");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass this class. Good luck taking it next time! ")
         }
     }
 }
