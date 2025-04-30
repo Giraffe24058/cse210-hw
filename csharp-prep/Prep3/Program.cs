96a4d42ef021d94632747d39caed9d4f11ee76ec
Giraffe24058 <all24058@byui.edu>
2025-04-30 15:52:36 -0600

    ask if they want to play the number guessign game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -11,48 +11,56 @@ class Program
     int guess = -1;
     int guessCount = 0;
 
-    do
-    {
-        Console.Write("Guess a number from 1 to 1001: ");
-        guess = int.Parse(Console.ReadLine());
+    Console.Write("Do you want to play the number guessing game? ");
+    yesOrNo = Console.ReadLine();
 
-        if (magicNumber > guess)
+    while yesOrNo == "yes";
+    {
+            
+        do
         {
-            Console.WriteLine("It's higher than that!");
-        }
-        else if (magicNumber < guess)
-        {
-            Console.WriteLine("It's lower than that!");
+            Console.Write("Guess a number from 1 to 1001: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("It's higher than that!");
             }
-        else
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("It's lower than that!");
+                }
+            else
+            {
+                Console.WriteLine($"You guessed it! It was {magicNumber}!");
+            }
+            ++ guessCount;
+
+        } while (guess != magicNumber);
+            
+        if (guessCount == 1) // This means that they guessed it on the first try!
         {
-            Console.WriteLine($"You guessed it! It was {magicNumber}!");
+            Console.Write($"Amazing!!! You guessed it on your first try!");
         }
-        ++ guessCount;
+        else if (guessCount < 15) // This means that it took less than 20 guesses
+        {
+            Console.Write($"Way to go! It only took you {guessCount} guesses!");
+            }
+        else if (guessCount < 21) // This means that it took less than 20 guesses
+        {
+            Console.Write($"Good job! It only took you {guessCount} guesses!");
+            }
+        else if (guessCount < 51) // This means that it took less than 50 guesses
+        {
+        Console.Write($"It took you {guessCount} guesses!");
+            }
+        else // This should mean that it took over 50 guesses
+        {
+            Console.Write($"It took you {guessCount} guesses! Can you get it in less than that next time?");
+        }
 
-    } while (guess != magicNumber);
-        
-     if (guessCount == 1) // This means that they guessed it on the first try!
-    {
-        Console.Write($"Amazing!!! You guessed it on your first try!");
-    }
-    else if (guessCount < 15) // This means that it took less than 20 guesses
-    {
-        Console.Write($"Way to go! It only took you {guessCount} guesses!");
-        }
-    else if (guessCount < 21) // This means that it took less than 20 guesses
-    {
-        Console.Write($"Good job! It only took you {guessCount} guesses!");
-        }
-    else if (guessCount < 51) // This means that it took less than 50 guesses
-    {
-    Console.Write($"It took you {guessCount} guesses!");
-        }
-    else // This should mean that it took over 50 guesses
-    {
-        Console.Write($"It took you {guessCount} guesses! Can you get it in less than that next time?");
-    }
 
+    }
 
     }
 }
