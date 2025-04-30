eaa05b27b0bd6fc588b708be6b6172568543d3de
Giraffe24058 <all24058@byui.edu>
2025-04-30 14:57:06 -0600

    make it a do while function, add if else if, else for higher lower and guessed it!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -10,11 +10,33 @@ class Program
 
     int guess = -1;
 
-     while (guess != magicNumber)
+
+    do
+    {
+    Console.Write("Do you want to continue? ");
+    response = Console.ReadLine();
+    } while (guess != magicNumber);
+
     {
         Console.WriteLine("Guess a number: ");
         guess = int.Parse(Console.ReadLine());
+    while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
 
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
 
 
     }
