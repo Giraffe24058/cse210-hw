498f0ff05af3e0a9a9d01ad368eb365e7fef6482
Giraffe24058 <all24058@byui.edu>
2025-04-30 15:45:33 -0600

    Add extra if else statements for the number fo guesses to encourage the user next time in their guessCount number.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -32,7 +32,26 @@ class Program
 
     } while (guess != magicNumber);
         
-    Console.WriteLine($"It only took you {guessCount} guesses!");
+         if (guessCount == 1) // This means that they guessed it on the first try!
+        {
+            Console.Write($"Amazing!!! You guessed it on your first try!");
+        }
+        else if (guessCount < 11) // This means that it took less than 20 guesses
+        {
+            Console.Write($"Way to go! It only took you {guessCount} guesses!");
+            }
+        else if (guessCount < 21) // This means that it took less than 20 guesses
+        {
+            Console.Write($"Good job! It only took you {guessCount} guesses!");
+            }
+        else if (guessCount < 51) // This means that it took less than 50 guesses
+        {
+            Console.Write($"It took you {guessCount} guesses!");
+            }
+        else // This should mean that it took over 50 guesses
+        {
+            Console.Write($"It took you {guessCount} guesses! Can you get it in less than that next time?");
+        }
 
     }
 }
