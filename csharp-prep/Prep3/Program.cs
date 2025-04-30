32050d556f017fd4d5250356d3e8a0fe17853ea0
Giraffe24058 <all24058@byui.edu>
2025-04-30 14:36:46 -0600

    started making random generator

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+    Random randomGenerator = new Random();
+    int magicNumber = randomGenerator.Next(1, 101);
+
+    int guess = -1;
+
+     while (guess != magicNumber)
+    {
+        Console.WriteLine("Guess a number: ");
+        guess = int.Parse(Console.ReadLine());
+
+
+
+    }
+
+
+
+
+
+
+
+
     }
 }
