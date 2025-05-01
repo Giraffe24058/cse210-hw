269397b97a1d29771eef0cc7494aaa17df719ba4
Giraffe24058 <all24058@byui.edu>
2025-04-30 20:15:36 -0600

    switch back to while do while,

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -14,26 +14,27 @@ class Program
     Console.Write("Do you want to play the numbers game? (Y/N)  ");
     string response = Console.ReadLine();
 
-    while (guess != magicNumber)
-    {
-        Console.Write("Guess a number from 1 to 1001: ");
-        guess = int.Parse(Console.ReadLine());
 
-        if (magicNumber > guess)
+    while (response.ToUpper() == "Y")
+        do
         {
-            Console.WriteLine("It's higher than that!");
-        }
-        else if (magicNumber < guess)
-        {
-            Console.WriteLine("It's lower than that!");
+            Console.Write("Guess a number from 1 to 1001: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("It's higher than that!");
             }
-        else
-        {
-            Console.WriteLine($"You guessed it! It was {magicNumber}! ");
-        }
-        ++ guessCount;
-
-    }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("It's lower than that!");
+                }
+            else
+            {
+                Console.WriteLine($"You guessed it! It was {magicNumber}! ");
+            }
+            ++ guessCount;
+        } while (guess != magicNumber);
         
     Console.WriteLine($"It took you {guessCount} guesses! ");
         
