ace5e147f72367544cd0f43be806c7599207521b
Giraffe24058 <all24058@byui.edu>
2025-04-30 20:12:04 -0600

    get rid of extra if statements, switch to just one statements of guesCounts

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -35,27 +35,8 @@ class Program
 
     }
         
-    if (guessCount == 1) // This means that they guessed it on the first try!
-    {
-        Console.WriteLine($"Amazing!!! You guessed it on your first try! ");
-    }
-    else if (guessCount < 15) // This means that it took less than 20 guesses
-    {
-        Console.WriteLine($"It only took you {guessCount} guesses! Way to go! ");
-        }
-    else if (guessCount < 21) // This means that it took less than 20 guesses
-    {
-        Console.WriteLine($"Good job! It only took you {guessCount} guesses! ");
-        }
-    else if (guessCount < 51) // This means that it took less than 50 guesses
-    {
     Console.WriteLine($"It took you {guessCount} guesses! ");
-        }
-    else // This should mean that it took over 50 guesses
-    {
-        Console.WriteLine($"It took you {guessCount} guesses! Can you get it in less than that next time? ");
-    }
-
+        
 
 
     }
