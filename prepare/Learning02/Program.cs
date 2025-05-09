31384a25e9645a4a3c68610223a61a335fe0a81d
Giraffe24058 <all24058@byui.edu>
2025-05-08 18:54:52 -0600

    deleted extra spaces, added new instance of the Resume class. added the two jobs I made earlier to resume. Display Resume Details

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,13 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job ();
+        Job job1 = new Job();
         job1._jobTitle = "Software Developer";
         job1._company = "Amazon";
         job1._startYear = 2018;
         job1._endYear = 2022;
         
-        Job job2 = new Job ();
+        Job job2 = new Job();
         job2._jobTitle = "Cashier";
         job2._company = "Walmart";
         job2._startYear = 2010;
@@ -19,6 +19,14 @@ class Program
 
         job1.DisplayJobDetails();
         job2.DisplayJobDetails();
+
+        Resume resume = new Resume();
+        resume._name = "Marisabelle";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResumeDetails();
+
 
     }
 }
