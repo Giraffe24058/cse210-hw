5229c56e1c23303903abf56a8a682279a6c84c4e
Giraffe24058 <all24058@byui.edu>
2025-05-08 18:48:13 -0600

    add public class resume, add _name, add for the list of Jobs

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel;
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+    public void DisplayResumeDetails()
+    {
+        Console.Write($"Name: {_name}");
+        Console.Write($"Name: {_name}");
+        for Job job in _jobs:
+        {
+            job.DisplayJobDetails();
+        }
+
+    }
+}
