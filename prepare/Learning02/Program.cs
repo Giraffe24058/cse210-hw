6ff56751527ff79fbfd7ba01219253deb72b3ea2
Giraffe24058 <all24058@byui.edu>
2025-05-08 18:34:43 -0600

    add job2. s and the details

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,16 +1,24 @@
 using System;
 
-public class Program
+class Program
 {
-
     static void Main(string[] args)
     {
-        Job job1._jobTitle = "Hairstyler";
+        Job job1 = new Job;
+        job1._jobTitle = "Software Developer";
+        job1._company = "Amazon";
+        job1._startYear = 2018;
+        job1._endYear = 2022;
+        
+        Job job2 = new Job;
+        job2._jobTitle = "Cashier";
+        job2._company = "Walmart";
+        job2._startYear = 2010;
+        job2._endYear = 2017;
     
 
 
 
-    
         Console.WriteLine("Hello Learning02 World!");
     }
 }
