989de0be23d644d8c078248da6b108832220e66b
Giraffe24058 <all24058@byui.edu>
2025-05-08 18:37:49 -0600

    added missing () s

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,13 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job;
+        Job job1 = new Job ();
         job1._jobTitle = "Software Developer";
         job1._company = "Amazon";
         job1._startYear = 2018;
         job1._endYear = 2022;
         
-        Job job2 = new Job;
+        Job job2 = new Job ();
         job2._jobTitle = "Cashier";
         job2._company = "Walmart";
         job2._startYear = 2010;
