6b36d3e71c282f3c3b8b7538f49e048bbe229f2e
Giraffe24058 <all24058@byui.edu>
2025-05-14 14:53:41 -0600

    move Entry program to develop02 folder. fix void WriteEntry to declare a body

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+class Entry
+{
+    public string _dateTime;
+    public string _entry;
+
+    public void WriteEntry()
+    {
+        Console.WriteLine("Hello Develop02 World!");
+    }
+}
