6d24dd056c62db130e09bc75203a592bc5b2fe06
Giraffe24058 <all24058@byui.edu>
2025-05-14 14:18:47 -0600

    writing in Program.cs, I started writing out the outline to the Journal program.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+public class Entry
+{
+    public string _dateTime
+    public string _entry
+
+    public void WriteEntry()
+}
+
+public class Journal
+{
+    public List<Entry> _Entries
+    public string _fileName
+    public void AddEntry()
+    public void Display()
+    public void SaveToFile()
+    public void LoadFromFile()
+    public void QuitProgram()
+}
+
+
