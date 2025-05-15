6f96d854ac17b26d1d37f586fb95697021ce29a4
Giraffe24058 <all24058@byui.edu>
2025-05-15 15:23:14 -0600

    Start writing the prompts list

--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -3,7 +3,14 @@ using System;
 
 class Prompt 
 {
-    public List<Prompt>Prompts;
+    public List<string> Prompts = new List<string>
+    {
+        "How do I feel about my efforts today?",
+        "Who did I interact with today?",
+        "Did I learn anything new today?",
+        "What books am I looking forward to reading?",
+        "Where have I seen something good in my life?",
+    };
     void DisplayPrompts()
     {
         Console.Write(" ");
