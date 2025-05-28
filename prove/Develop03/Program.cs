53dde15b499f7477e54fed047917f2eed7f3db4a
Giraffe24058 <all24058@byui.edu>
2025-05-27 19:07:34 -0600

    put in the changes, transition from the plant uml diagram, put into coding. Moved this folder back into the cse210folder, saved from the recycle bin on accident.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference, "Trust ...");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetRenderedText());
+            Console.WriteLine("\nPress Enter to hide more words or type 'quit' to exit.");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit" || scripture.IsCompletelyHidden())
+                break;
+
+            scripture.HideRandomWords(3);
+        }
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,6 +1,52 @@
 using System;
-
+using System.Collections.Generic;
 public class Scripture
 {
+    private Reference _reference;
+    private List<Word> _words;
 
-}
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] wordArray = text.Split(' ');
+        foreach (string word in wordArray)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random rand = new Random();
+        int hiddenCount = 0;
+
+        while (hiddenCount < numberToHide)
+        {
+            int index = rand.Next(_words.Count);
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+        }
+    }
+
+    public string GetRenderedText()
+    {
+        string renderedWords = string.Join(" ", _words.ConvertAll(w => w.GetRenderedText()));
+        return $"{_reference.GetDisplayText()} {renderedWords}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+                return false;
+        }
+        return true;
+    }
+
+};
