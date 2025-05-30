569ded19003f667a62ce079233deda21d86e3349
Giraffe24058 <all24058@byui.edu>
2025-05-29 18:15:36 -0600

    added scripture passages - doctrinal mastery passages from seminary.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,11 +7,23 @@ class Program
 
         List<Scripture> scriptureLibrary = new List<Scripture>
         {
-            new Scripture(new Reference("Proverbs", 1, 2, 3), "..."),
-            new Scripture(new Reference("Proverbs", 1, 2, 3), "..."),
-            new Scripture(new Reference("Proverbs", 1, 2, 3), "..."),
-            new Scripture(new Reference("Proverbs", 1, 2, 3), "..."),
-            new Scripture(new Reference("Proverbs", 1, 2, 3), "..."),
+            new Scripture(new Reference("Proverbs", 3, 11, 12), " My son, despise not the chastening of the Lord; neither be weary of his correction: For whom the Lord loveth he correcteth; even as a father the son in whom he delighteth."),
+            new Scripture(new Reference("Proverbs", 3, 13), "Happy is the man that findeth wisdom, and the man that getteth understanding."),
+            new Scripture(new Reference("Proverbs", 28, 13), "He that covereth his sins shall not prosper: but whoso confesseth and forsaketh them shall have mercy."),
+            new Scripture(new Reference("Alma", 7, 11, 13), "And he shall go forth, suffering pains and afflictions and temptations of every kind."),
+            new Scripture(new Reference("Alma", 34, 9, 10), "There must be an atonement made, … an infinite and eternal sacrifice."),
+            new Scripture(new Reference("Alma", 39, 9), "Go no more after the lusts of your eyes."),
+            new Scripture(new Reference("Alma", 41, 10), "Wickedness never was happiness."),
+            new Scripture(new Reference("Helaman", 5, 12), "It is upon the rock of our Redeemer … that ye must build your foundation."),
+            new Scripture(new Reference("3 Nephi", 11, 10, 11), "I have suffered the will of the Father in all things from the beginning."),
+            new Scripture(new Reference("3 Nephi", 12, 48), "Be perfect even as I, or your Father who is in heaven is perfect."),
+            new Scripture(new Reference("3 Nephi", 27, 20), "Come unto me and be baptized … that ye may be sanctified by the reception of the Holy Ghost."),
+            new Scripture(new Reference("Ether", 12, 6), "Ye receive no witness until after the trial of your faith."),
+            new Scripture(new Reference("Ether", 12, 27), "If men come unto me … then will I make weak things become strong unto them."),
+            new Scripture(new Reference("Moroni", 7, 45, 48), "Charity is the pure love of Christ."),
+            new Scripture(new Reference("Moroni", 10, 4, 5), "Ask with a sincere heart, with real intent, having faith in Christ … [and] by the power of the Holy Ghost ye may know the truth of all things.")
+
+            
         };
 
 
