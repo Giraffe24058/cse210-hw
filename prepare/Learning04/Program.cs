0418e5228e052eee8d43602109f2ff7b84f41311
Giraffe24058 <all24058@byui.edu>
2025-05-29 22:28:57 -0600

    In the MathAssingment class, inherit from the Assingemnt parent class. Added the textbook section and problems. Call the base function, make strign GetHomeworkList and return the details of the assignmnt. Also, test this in the main function by creating new MathAssignment and the details - the GetSummary().

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -7,5 +7,8 @@ class Program
         Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
         Console.WriteLine(assignment.GetSummary());
 
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
     }
 }
