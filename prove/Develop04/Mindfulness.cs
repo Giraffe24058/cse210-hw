41934b640cd37de8879f3afe3b33a4164530433a
Giraffe24058 <all24058@byui.edu>
2025-06-07 21:24:09 -0600

    I started with the outline of the code, with teh structures and methods.

--- prove/Develop04/Mindfulness.cs
+++ prove/Develop04/Mindfulness.cs
@@ -1,26 +1,94 @@
 using System;
 
-public class Mindfulness
+abstract class Mindfulness
 {
-    static void Main(string[] args)
+    protected string _className;
+    protected string _classDescription;
+    protected int _time;
+
+    public void Start()
     {
-        Console.WriteLine("Hello Develop04 World! ");
+        // dsplay welcome message and set duration
+    }
+
+    public void End()
+    {
+        // dsplay completion message
+    }
+
+    protected void Spinner(int seconds)
+    {
+        // show spinner
+    }
+
+    protected void Countdown(int seconds)
+    {
+        // countdown
+    }
+
+    public abstract void RunActivity();
+}
+
+class Breathing : Mindfulness
+{
+    public Breathing()
+    {
+        // Set _className and _classDescription
+    }
+
+    public override void RunActivity()
+    {
+        Start();
+        // Alternate "Breathe in..." and "Breathe out..." with countdowns
+        End();
     }
 }
 
-public class Breathing  Mindfulness;
+class Reflection : Mindfulness
 {
-     
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    public Reflection()
+    {
+        // Initialize _className, _classDescription, _prompts, and _questions
+    }
+
+    public override void RunActivity()
+    {
+        Start();
+        // Show a random prompt
+        End();
+    }
 }
 
-
-public class Listing : Mindfulness;
+class Listing : Mindfulness
 {
-     
+    private List<string> _prompts;
+
+    public Listing()
+    {
+        // Initialize _className, _classDescription, and _prompts
+    }
+
+    public override void RunActivity()
+    {
+        Start();
+        // Show a random prompt, countdown to start, collect user input until time runs out
+        // display number of items listed
+        End();
+    }
 }
 
-
-public class Reflection : Mindfulness;
+// Main program
+class Program
 {
-     
+    static void Main()
+    {
+        while (true)
+        {
+            // display menu, get user choice, create appropriate activity object
+            // call RunActivity() , return to menu or exit
+        }
+    }
 }
