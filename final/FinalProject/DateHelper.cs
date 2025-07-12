a08dadb8c0d78676e316422daf4359cd68b4e865
Giraffe24058 <all24058@byui.edu>
2025-07-11 21:09:01 -0600

    fix DateHelper

--- final/FinalProject/DateHelper.cs
+++ final/FinalProject/DateHelper.cs
@@ -5,16 +5,3 @@ public static class DateHelper
         return (date - DateTime.Now).Days;
     }
 }
-public abstract class Document
-{
-    public string Title { get; set; }
-    public DateTime Date { get; set; }
-
-    protected Document(string title, DateTime date)
-    {
-        Title = title;
-        Date = date;
-    }
-
-    public abstract void Export();
-}
